cf05a91934c97e03716ff7105b94bca0bcd1814d
j4vi3r4 <ja.basaez@alumnos.duoc.cl>
2020-07-15 21:58:36 -0400

    splashscreen y cambio de nombre app

--- Totem_SigloXXI.Android/MainActivity.cs
+++ Totem_SigloXXI.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Totem_SigloXXI.Droid
 {
-    [Activity(Label = "Totem_SigloXXI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Totem_SigloXXI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
